16c1a4456efb5c1b8897f53ad984d6900ac93b28
Matthieu Mercier (aera128) <matthmercier8@gmail.com>
2019-10-23 22:45:37 +0200

    commit

--- App/Controller/ImageController.php
+++ App/Controller/ImageController.php
@@ -9,8 +9,12 @@ use Core\View;
 class ImageController extends Controller{
 
     public function showAction($url){
-        dump(json_decode(shell_exec("..\\Resources\\exiftool\\windows\\exiftool.exe -json -g2 ..\\public\\images\\".$url)));
-        View::renderTwig('images/show.html.twig');
+        $meta = json_decode(shell_exec("..\\Resources\\exiftool\\windows\\exiftool.exe -json -g2 ..\\public\\images\\" . $url), true);
+        View::renderTwig('images/show.html.twig', array(
+            "url" => $url,
+            "meta" => $meta[0],
+        ));
+        dump($meta[0]);
     }
 
     public function addAction()
